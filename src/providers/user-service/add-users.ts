7bc7426123aaa274261a25c717336a1d31707851
anusit1234 <not_anusit@hotmail.com>
2018-03-30 11:45:38 +0700

    แก้ url

--- src/providers/user-service/add-users.ts
+++ src/providers/user-service/add-users.ts
@@ -23,7 +23,7 @@ export class AddUserProvider {
       let host = sessionStorage.getItem("host");
 
       return new Promise<User>(resolve=>{
-        this.http.post(host+'/services/users/getAddUser',JSON.stringify({data:user}),
+        this.http.post(host+'/services/users/addUser',JSON.stringify({data:user}),
         {headers: new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded')}
     )
         .subscribe(user=>{
